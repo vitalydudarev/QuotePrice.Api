a05ce221bb4f67f90791861a1d99a46b287e1f0f
Vitaly Dudarau <vitalydudarev@gmail.com>
2023-02-20 01:21:03 +0300

    Added Sqlite migrations.

---
+++ Migrations/QuotePrice.Sqlite/Migrations/20230219221715_InitialMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuotePrice.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "quotes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Pair = table.Column<string>(type: "TEXT", nullable: false),
+                    Source = table.Column<string>(type: "TEXT", nullable: false),
+                    Timestamp = table.Column<double>(type: "REAL", nullable: true),
+                    Bid = table.Column<double>(type: "REAL", nullable: true),
+                    Ask = table.Column<double>(type: "REAL", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_quotes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "quotes");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # QuotePrice.Api
+
+## Run migrations:
+Migrations must be run the root folder:
+```console
+dotnet ef migrations add InitialMigration -s QuotePrice.Api --project ./Migrations/QuotePrice.Sqlite -- --DatabaseType Sqlite
+dotnet ef migrations add InitialMigration -s QuotePrice.Api --project ./Migrations/QuotePrice.Postgres -- --DatabaseType Postgres
+```
